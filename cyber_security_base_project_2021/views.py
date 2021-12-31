c458e599983440d248f101df59034d90019fd434
Antti Ollikkala <antti@huom.io>
2021-12-31 16:50:38 +0200

    introduced sql injection bug

--- cyber_security_base_project_2021/views.py
+++ cyber_security_base_project_2021/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.db import connection
 from .model import Note, User
 
 
@@ -11,6 +12,7 @@ def index(request):
 
 def logout(request):
     request.session["user_id"] = None
+    request.session["username"] = None
     return HttpResponseRedirect("/login")
 
 
@@ -20,6 +22,7 @@ def login(request):
         user = User.objects.filter(username=request.GET.get('username'), password=request.GET.get('password')).first()
         if user is not None:
             request.session['user_id'] = user.id
+            request.session['username'] = user.username
             return HttpResponseRedirect("/notes")
         return HttpResponse('Unauthorized', status=401)
     template = loader.get_template('login.html')
@@ -34,7 +37,7 @@ def notes(request):
         new_note = Note(user_id=int(request.GET.get('user_id')), content=request.GET.get('note_content'))
         new_note.save()
         return HttpResponseRedirect("/notes")
-    all_notes = Note.objects.filter(user_id=request.session.get("user_id")).all()
+    all_notes = get_user_notes(request.session["username"])
     template = loader.get_template('notes.html')
     return HttpResponse(template.render({'notes': all_notes, 'user': user}, request))
 
@@ -46,6 +49,17 @@ def register(request):
         u.save()
         u2 = User.objects.filter(username=request.GET.get('username')).first()
         request.session['user_id'] = u2.id
+        request.session['username'] = u2.username
         return HttpResponseRedirect("/notes")
     template = loader.get_template('register.html')
     return HttpResponse(template.render({}, request))
+
+
+def get_user_notes(username):
+    with connection.cursor() as cursor:
+        query_string = "SELECT * FROM cyber_security_base_project_2021_note n LEFT JOIN cyber_security_base_project_2021_user u ON n.user_id = u.id WHERE u.username = '%s'" % username
+        cursor.execute(query_string)
+        results = []
+        for note in cursor.fetchall():
+            results.append({'content': note[1]})
+        return results
